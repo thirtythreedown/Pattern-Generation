024e3fb26d5ee4dfd54c5e7c83ab6b239ea936df
thirtythreedown <32502009+thirtythreedown@users.noreply.github.com>
2020-02-21 17:02:25 -0600

    name-saving function, general cleanup

--- list_saver_function_test.py
+++
@@ -1,7 +0,0 @@
-##See Automate the boring stuff with Python p.299/505, "Write the results to a file"
-
-def list_saver(cleaned_list):
-    """Module compares list of generated_names with data in saved_projects and removes redundancies"""  
-    for value in cleaned_list:
-        print("Saving " + item + " to writing spreadsheet")
-    print("Done saving!")
